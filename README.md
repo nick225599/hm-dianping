bf5cc3e7e36fbe6b0276c5367d1bc8544396518b
nick225599 <nick225599@163.com>
2022-11-17 21:16:06 +0800

    店铺详情缓存

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hm-dianping
+
+d:
+cd \Program Files\nginx-1.18.0\
+start .\nginx.exe
+http://localhost:8080/index.html
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,14 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.time.Duration;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +26,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Shop getById(Serializable id) {
+        String shopJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
+        if (StringUtils.isNotEmpty(shopJson)) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        } else {
+            Shop shop = super.getById(id);
+            if (null != shop) {
+                shopJson = JSONUtil.toJsonStr(shop);
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, shopJson,
+                        Duration.ofMinutes(CACHE_SHOP_TTL));
+            }
+            return shop;
+        }
+    }
 }
